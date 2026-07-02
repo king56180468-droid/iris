6c1c948f20785aa76798a0cb225f87c518bdd313
king56180468-droid <king56180468@gmail.com>
2026-07-02 09:11:29 +0100

    'test2'

--- lib/iris/util.py
+++ lib/iris/util.py
@@ -625,8 +625,7 @@ def dummy_function():
 
 def dummy_function2():
     """Dummy function."""
-    return "blah"
-
+    return "blahhhh"
 
 def reverse(cube_or_array, coords_or_dims):
     """Reverse the cube or array along the given dimensions.
